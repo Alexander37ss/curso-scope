b33729a93e05ec824b7e52745be82e66f281b2e1
Alexander37ss <alexanderpalazuelos37@gmail.com>
2023-03-28 11:50:56 -0700

    segunda clase variables declaradas en funciones

---
+++ function.js
@@ -0,0 +1,9 @@
+function greeeting(){
+    let userName = 'El pepe';
+    console.log(userName);
+    
+    if(userName === 'El pepe'){
+        console.log('Hola '+ userName);
+    }
+}
+greeeting();
