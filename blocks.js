f8d1b6b62ee317871b3b17a0d2091b26a66e5d1d
Alexander37ss <alexanderpalazuelos37@gmail.com>
2023-03-28 11:59:07 -0700

    Tercera clase block scope

---
+++ blocks.js
@@ -0,0 +1,11 @@
+function fruits(){
+    if(true){
+        var fruit1 = 'Kiwi';
+        let fruit2 = 'Toronja';
+        const fruit3 = 'Sandia';
+    }
+    console.log(fruit1);
+    console.log(fruit2);
+    console.log(fruit3);
+}
+fruits();
