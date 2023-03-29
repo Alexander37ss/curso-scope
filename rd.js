039852e70f947c1da7f58c331de178f6e177acc7
Alexander37ss <alexanderpalazuelos37@gmail.com>
2023-03-28 21:24:08 -0700

    perdí la cuenta

---
+++ rd.js
@@ -0,0 +1,21 @@
+//Variables tipo var
+var firstName;
+firstName = 'Javier';
+console.log(firstName);
+
+var lastName = 'David';
+lastName = 'Tilin';
+console.log(lastName);
+
+var secondName  = 'David';
+var secondName  = 'Julio';
+console.log(secondName);
+
+//Variables tipo let
+let fruit = 'Apple';
+fruit = 'Kiwi';
+
+//Constantes
+const animal = 'Leon';
+animal = 'Gato';
+console.log(animal);
